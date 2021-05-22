fd1f7600dc492ca59a6e6dba0943d086654691e3
David Blundell <david.blundell@ratehub.ca>
2021-05-22 19:59:09 -0400

    Added some proper events

--- app/events/index.js
+++ app/events/index.js
@@ -1,9 +1,16 @@
-import test from './test'
-import test2 from './test2'
-import test3 from './test3'
+import security1 from './security1'
+import security2 from './security2'
+import security3 from './security3'
+import security4 from './security4'
+import security5 from './security5'
+import security6 from './security6'
+
 
 export default [
-    test,
-    test2,
-    test3,
+    security1,
+    security2,
+    security3,
+    security4,
+    security5,
+    security6,
 ]
